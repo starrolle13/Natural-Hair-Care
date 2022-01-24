47c78dd411c231f68d48d054c5f4e434bfbc4d9f
starrolle13 <starnesharolle13@gmail.com>
2022-01-24 09:42:20 -0800

    cleaned up css, fixed logo

--- natural-hair-care/src/Pricing.js
+++ natural-hair-care/src/Pricing.js
@@ -1,9 +1,11 @@
 import NavBar from './NavBar';
 import Footer from './Footer';
+import Spacer from './Spacer';
 function Pricing() {
   return (
     <div className="App">
       <NavBar />
+      <Spacer />
       <Footer />
     </div>
   );
